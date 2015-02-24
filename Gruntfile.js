3ed2eba214fa324e04efe9e12bf2f9d16edae796
Michael Szymczak <contact@michaelszymczak.com>
2015-02-24 23:43:20 +0000

    added directories to clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,6 +91,8 @@ module.exports = function (grunt) {
           dot: true,
           src: [
             '<%= yeoman.dist %>/*',
+            '<%= yeoman.app %>/css/*',
+            '.sass-cache/*',
             // Running Jekyll also cleans the target directory.  Exclude any
             // non-standard `keep_files` here (e.g., the generated files
             // directory from Jekyll Picture Tag).
